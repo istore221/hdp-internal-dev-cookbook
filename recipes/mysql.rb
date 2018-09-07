503f40749e4c5cc11389887248071bedd6af2a00
Kalana Thejitha <kalana@n-able.biz>
2018-09-07 13:55:52 +0530

    mysql hostname added

--- recipes/mysql.rb
+++ recipes/mysql.rb
@@ -3,10 +3,3 @@
 # Recipe:: mysql
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
-
-mysql_service 'foo' do
-  port '3306'
-  version '5.5'
-  initial_root_password 'mypassword'
-  action [:create, :start]
-end
